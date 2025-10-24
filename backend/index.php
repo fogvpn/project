83d339d93f5d4d81af94b7ee9886d9c36037060e
FOGGY <support@fogconfig.click>
2025-10-25 01:16:37 +0300

    Add files via upload

--- backend/index.php
+++ backend/index.php
@@ -11,5 +11,6 @@ require __DIR__ . '/src/flight/me.php';
 require __DIR__ . '/src/flight/payments.php';
 require __DIR__ . '/src/flight/logout.php';
 require __DIR__ . '/src/flight/config.php';
+require __DIR__ . '/src/flight/traffic.php';
 
 Flight::start();
